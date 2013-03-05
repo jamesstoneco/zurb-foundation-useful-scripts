70bfe76cf0618d3c8cb5f37855d7fff32e6f1022
James Stone <jamesmanofstone@gmail.com>
2013-03-05 13:15:21 -0800

    first commit

---
+++ compassf_git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Create a new foundation project from git, usage: foundation_create_git project_name git_url
+# use `bundle exec compass watch` while working on your project
+mkdir $1
+cd $1
+touch Gemfile
+echo "source \"https://rubygems.org\"" >> Gemfile
+echo "gem \"zurb-foundation\", :git => \"$2\"" >> Gemfile
+echo "gem \"compass\""
+bundle install
+bundle exec compass create . -r zurb-foundation --using foundation
---
+++ compassf_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Create a new foundation project with a specific version, usage: foundation_create_git project_name 3.2.5 
+# use `bundle exec compass watch` while working on your project
+# make sure you install the proper gem first with, `gem install zurb-foundation --version 3.2.5 
+mkdir $1
+cd $1
+touch Gemfile
+echo "source \"https://rubygems.org\"" >> Gemfile
+echo "gem \"zurb-foundation\", \"$2\"" >> Gemfile
+echo "gem \"compass\""
+bundle install
+bundle exec compass create . -r zurb-foundation --using foundation
